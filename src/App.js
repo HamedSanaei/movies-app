79821cf3c157635c8002b48015300ea0a79ed802
hamedsanaei <hamedsan76@gmail.com>
2021-02-11 20:00:06 +0330

    complete sec 6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
+
 import Movies from "./components/movies";
+import Customers from "./components/common/customers";
+import Rentals from "./components/common/rentals";
+import NavBar from "./components/common/navBar";
+import MovieForm from "./components/common/movieForm";
+import { Switch, Route, Redirect } from "react-router-dom";
+import NotFound from "./components/common/notFound";
 
 class App extends Component {
   render() {
     return (
-      <main className="container  ">
-        <Movies></Movies>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" componet={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
