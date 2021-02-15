f92325b54588c03334724b14163415164e728f0e
hamedsanaei <hamedsan76@gmail.com>
2021-02-15 04:51:35 +0330

    complete lesson 122

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -12,26 +12,50 @@ class LoginForm extends Component {
   // }
 
   validate = () => {
-    return { username: "Username is required." };
+    const errors = {};
+
+    const { account } = this.state;
+    if (account.username.trim() === "")
+      errors.username = "Username is required.";
+
+    if (account.password.trim() === "")
+      errors.password = "Password is required.";
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
   handleSubmit = (e) => {
     e.preventDefault();
 
     const errors = this.validate();
-    this.setState({ errors });
+    console.table(errors);
+    this.setState({ errors: errors || {} });
     if (errors) return;
     // const username = this.username.current.value;
     // call the server
     //console.log(username);
   };
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+      // ....
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+      // ....
+    }
+  };
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessages = this.validateProperty(input);
+    if (errorMessages) errors[input.name] = errorMessages;
+    else delete errors[input.name];
     const account = { ...this.state.account };
     account[input.name] = input.value;
 
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div className="container">
         <h1>Login</h1>
@@ -41,6 +65,7 @@ class LoginForm extends Component {
             name="username"
             value={account.username}
             label="Username"
+            error={errors.username}
             onChange={this.handleChange}
           />
           {/* <div className="form-group">
@@ -60,6 +85,7 @@ class LoginForm extends Component {
             name="password"
             value={account.password}
             label="Password"
+            error={errors.password}
             onChange={this.handleChange}
           />
           <button className="btn btn-primary">Login</button>
